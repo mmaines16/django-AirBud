3b6bf25d44828afea398e048c753416fd141081e
Mason Maines (SunGard-Lake Mary) <mason.maines@sungardps.com>
2016-10-17 18:08:40 -0400

    Add Gust to model and add Wind Data Model to the Django Admin with the date and time as the unicode string

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from models import WindData
+
 # Register your models here.
+class WindDataAdmin(admin.ModelAdmin):
+    readonly_fields = (
+    'wind_direction',
+    'variable_wind_direction_max',
+    'variable_wind_direction_min',
+    'wind_speed',
+    'wind_gust',
+    'time_stamp'
+    )
+    class Meta:
+        model = WindData
+        exclude = ()
+
+admin.site.register(WindData, WindDataAdmin)
--- data/models.py
+++ data/models.py
@@ -5,5 +5,16 @@ from django.db import models
 # Create your models here.
 class WindData(models.Model):
     wind_direction = models.IntegerField(null=False, blank=True, default=0)
+    variable_wind_direction_max = models.IntegerField(null=False, blank=True, default=0)
+    variable_wind_direction_min = models.IntegerField(null=False, blank=True, default=0)
     wind_speed = models.DecimalField(null=False, blank=True, default=0.0, max_digits=3, decimal_places=2)
+    wind_gust = models.DecimalField(null=False, blank=True, default=0.0, max_digits=3, decimal_places=2)
     time_stamp = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+    def __unicode__(self):
+        date = str(self.time_stamp.date())
+        hour = str(self.time_stamp.time().hour)
+        minute = str(self.time_stamp.time().minute)
+        second = str(self.time_stamp.time().second)
+        microsecond = str(self.time_stamp.time().microsecond)
+        return "Date: " + date + " Time: " + hour + "-" + minute + "-" + second + " (" + microsecond + ")"
